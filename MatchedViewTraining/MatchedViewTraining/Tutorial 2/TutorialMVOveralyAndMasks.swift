3c8c64868c07945bd22514cbc591bd119c6b2d39
palant-dev <113528707+apalomba22@users.noreply.github.com>
2023-04-13 20:24:57 +0200

    fix preview error

---
+++ MatchedViewTraining/MatchedViewTraining/Tutorial 2/TutorialMVOveralyAndMasks.swift
@@ -0,0 +1,147 @@
+//
+//  TutorialMVOveralyAndMasks.swift
+//  MatchedViewTraining
+//
+//  Created by Antonio Palomba on 13/04/23.
+//
+
+import SwiftUI
+
+struct TutorialMVOveralyAndMasks: View {
+    @Namespace var namespace
+    @State var show = false
+
+    var body: some View {
+        ZStack {
+            if !show {
+
+                VStack {
+                    Spacer()
+
+                    VStack(alignment: .leading, spacing: 12) {
+                        Text("SwiftUI")
+                            .font(.largeTitle.weight(.bold))
+                            .matchedGeometryEffect(id: "header", in: namespace)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+
+                        Text("20 sections - 3 hours".uppercased())
+                            .font(.footnote.weight(.semibold))
+                            .matchedGeometryEffect(id: "subtitle", in: namespace)
+
+                        Text("Build an iOS app for iOS 15 with custom layouts, animations and...")
+                            .font(.footnote)
+                            .matchedGeometryEffect(id: "text", in: namespace)
+                    }
+                    .padding(20)
+                    .background(
+                        Rectangle()
+                            .fill(.ultraThinMaterial)
+                            .mask(
+                                RoundedRectangle(cornerRadius: 30, style: .continuous))
+                            .blur(radius: 30)
+                            .matchedGeometryEffect(id: "blur", in: namespace)
+
+                    )
+
+
+                }
+
+                .foregroundColor(.white)
+                .background(
+                    Image("Illustration 9")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .matchedGeometryEffect(id: "image", in: namespace)
+                )
+                .background(
+                    Image("Background5")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .matchedGeometryEffect(id: "background", in: namespace)
+                )
+                .mask {
+                    RoundedRectangle(cornerRadius: 30, style: .continuous)
+                        .matchedGeometryEffect(id: "mask", in: namespace)
+                }
+                .frame(height: 300)
+                .padding(20)
+
+            } else {
+                ScrollView {
+                    VStack {
+                        Spacer()
+                    }
+                    // We use this .frame() because now the spacer will grant the maximum space only vertically, and the text is now inside the overlay modifier so it will not grant the maximum width anymore
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 500)
+                    .foregroundColor(.black)
+                    .background(
+                        Image("Illustration 9")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .matchedGeometryEffect(id: "image", in: namespace)
+                    )
+                    .background(
+                        Image("Background5")
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .matchedGeometryEffect(id: "background", in: namespace)
+                    )
+                    .mask {
+                        RoundedRectangle(cornerRadius: 30, style: .continuous)
+                            .matchedGeometryEffect(id: "mask", in: namespace)
+                    }
+                    .overlay(
+                        VStack(alignment: .leading, spacing: 12) {
+                            Text("SwiftUI")
+                                .font(.largeTitle.weight(.bold))
+                                .matchedGeometryEffect(id: "header", in: namespace)
+                                .frame(maxWidth: .infinity, alignment: .leading)
+
+                            Text("20 sections - 3 hours".uppercased())
+                                .font(.footnote.weight(.semibold))
+                                .matchedGeometryEffect(id: "subtitle", in: namespace)
+
+                            Text("Build an iOS app for iOS 15 with custom layouts, animations and...")
+                                .font(.footnote)
+                                .matchedGeometryEffect(id: "text", in: namespace)
+
+                            Divider()
+
+                            HStack {
+                                Image(systemName: "person")
+                                    .fontWeight(.bold)
+                                    .padding(20)
+                                    .background(
+                                        .ultraThinMaterial, in: RoundedRectangle(cornerRadius: 18, style: .continuous))
+                                Text("Tought by Antonio Palomba")
+                                    .font(.footnote)
+                            }
+                        }
+                            .padding(20)
+                            .background(
+                                Rectangle()
+                                    .fill(.ultraThinMaterial)
+                                    .mask(
+                                        RoundedRectangle(cornerRadius: 30, style: .continuous))
+                                    .matchedGeometryEffect(id: "blur", in: namespace)
+                            )
+                            .offset(y: 250)
+                            .padding(20)
+                    )
+                }
+            }
+        }
+        .onTapGesture {
+            withAnimation (.spring(response: 0.6, dampingFraction: 0.8)) {
+                show.toggle()
+            }
+        }
+    }
+}
+
+struct TutorialMVOveralyAndMasks_Previews: PreviewProvider {
+    static var previews: some View {
+        TutorialMVOveralyAndMasks()
+    }
+}
---
+++ MatchedViewTraining/MatchedViewTraining/Tutorial-part-1/TutorialMVText.swift
@@ -0,0 +1,42 @@
+//
+//  TutorialMVText.swift
+//  MatchedViewTraining
+//
+//  Created by Antonio Palomba on 13/04/23.
+//
+
+import SwiftUI
+
+struct TutorialMVText: View {
+    @Namespace var namespace
+    @State var show = false
+
+    var body: some View {
+        ZStack {
+            // In this case we are going to use !show in order to have first the not active animetion contained in the first part of the if-conditional and then in the else the second part of the animation. But it's a matter of personal preferences
+            if !show {
+                Text("SwiftUI")
+                //Attention to the modifier stack, if you put this after .frame() will not work
+                    .matchedGeometryEffect(id: "title", in: namespace)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+            } else {
+                Text("SwiftUI")
+                //Also care for the formatting of the element because if we change the style this wont work
+                    .matchedGeometryEffect(id: "title", in: namespace)
+                    .frame(maxWidth: .infinity, alignment: .trailing)
+            }
+        }
+        .onTapGesture {
+            withAnimation {
+                show.toggle()
+            }
+        }
+    }
+}
+
+
+struct TutorialMVText_Previews: PreviewProvider {
+    static var previews: some View {
+        TutorialMVText()
+    }
+}
