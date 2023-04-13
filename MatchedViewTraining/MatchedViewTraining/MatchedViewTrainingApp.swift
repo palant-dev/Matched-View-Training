e5f0e10875a13021e81ed5c601e0d862ce61dd53
palant-dev <113528707+apalomba22@users.noreply.github.com>
2023-04-13 10:13:39 +0200

    initial push for xcode files

---
+++ MatchedViewTraining/MatchedViewTraining/MatchedViewTrainingApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatchedViewTrainingApp.swift
+//  MatchedViewTraining
+//
+//  Created by Antonio Palomba on 13/04/23.
+//
+
+import SwiftUI
+
+@main
+struct MatchedViewTrainingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
