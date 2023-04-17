c67bf19da0e530c34738d31bcd0f045de36f452f
palant-dev <113528707+apalomba22@users.noreply.github.com>
2023-04-17 18:44:08 +0200

    Added courseView

--- MatchedViewTraining/MatchedViewTraining/View/CourseView.swift
+++ MatchedViewTraining/MatchedViewTraining/View/CourseView.swift
@@ -8,13 +8,80 @@
 import SwiftUI
 
 struct CourseView: View {
+    var namespace: Namespace.ID
+    @Binding var show: Bool
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            VStack {
+                Spacer()
+            }
+            // We use this .frame() because now the spacer will grant the maximum space only vertically, and the text is now inside the overlay modifier so it will not grant the maximum width anymore
+            .frame(maxWidth: .infinity)
+            .frame(height: 500)
+            .foregroundColor(.black)
+            .background(
+                Image("Illustration 9")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .matchedGeometryEffect(id: "image", in: namespace)
+            )
+            .background(
+                Image("Background5")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .matchedGeometryEffect(id: "background", in: namespace)
+            )
+            .mask {
+                RoundedRectangle(cornerRadius: 30, style: .continuous)
+                    .matchedGeometryEffect(id: "mask", in: namespace)
+            }
+            .overlay(
+                VStack(alignment: .leading, spacing: 12) {
+                    Text("SwiftUI")
+                        .font(.largeTitle.weight(.bold))
+                        .matchedGeometryEffect(id: "header", in: namespace)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+
+                    Text("20 sections - 3 hours".uppercased())
+                        .font(.footnote.weight(.semibold))
+                        .matchedGeometryEffect(id: "subtitle", in: namespace)
+
+                    Text("Build an iOS app for iOS 15 with custom layouts, animations and...")
+                        .font(.footnote)
+                        .matchedGeometryEffect(id: "text", in: namespace)
+
+                    Divider()
+
+                    HStack {
+                        Image(systemName: "person")
+                            .fontWeight(.bold)
+                            .padding(20)
+                            .background(
+                                .ultraThinMaterial, in: RoundedRectangle(cornerRadius: 18, style: .continuous))
+
+                        Text("Tought by Antonio Palomba")
+                            .font(.footnote)
+                    }
+                }
+                    .padding(20)
+                    .background(
+                        Rectangle()
+                            .fill(.ultraThinMaterial)
+                            .mask(
+                                RoundedRectangle(cornerRadius: 30, style: .continuous))
+                            .matchedGeometryEffect(id: "blur", in: namespace)
+                    )
+                    .offset(y: 250)
+                    .padding(20)
+            )
+        }
     }
 }
 
 struct CourseView_Previews: PreviewProvider {
+    @Namespace static var namespace
     static var previews: some View {
-        CourseView()
+        CourseView(namespace: namespace, show: .constant(true))
     }
 }
